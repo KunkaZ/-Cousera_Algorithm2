b4c81506a53f54c43e941ad473611cd6b93353f7
Kun Zhang <kuna.zhang@gmail.com>
2022-04-16 19:15:44 -0700

    Wk-5 BurrowsWheeler.java finished

--- Wk_5/BurrowsWheeler.java
+++ Wk_5/BurrowsWheeler.java
@@ -25,14 +25,63 @@ public class BurrowsWheeler {
     }
 
     // apply Burrows-Wheeler inverse transform,
-// reading from standard input and writing to standard output
+    // reading from standard input and writing to standard output
     public static void inverseTransform() {
+        int R = 256;
         // input is first and t, we can sort t to get the first column of sorted suffixes
+        int first = BinaryStdIn.readInt();
+        String t = BinaryStdIn.readString();
+
+//        int first = 3;
+//        String t = "ARD!RCAAAABB";
+
+        char[] inputSortedChar = t.toCharArray();
+
+        int[] next = new int[t.length()];
+
+        int[] duplicatedCnts = new int[R + 1];
+
+        // use key indexed string sorting
+        for (int i = 0; i < t.length(); i++) {
+            duplicatedCnts[t.charAt(i) + 1]++;
+        }
+
+        for (int r = 0; r < R; r++) {
+            duplicatedCnts[r + 1] += duplicatedCnts[r];
+        }
+
+        for (int i = 0; i < t.length(); i++) {
+            int pos = duplicatedCnts[t.charAt(i)]++;
+            inputSortedChar[pos] = t.charAt(i);
+            next[pos] = i;  // i is the original index of sorted char
+        }
+//
+//        StdOut.println(Arrays.toString(next));
+//        StdOut.println("inputSortedChar" + Arrays.toString(inputSortedChar));
+
+
+        int lastId = first;
+        for (int i = 0; i < t.length(); i++) {
+            BinaryStdOut.write(inputSortedChar[lastId]);
+//            StdOut.println(next[first]);
+//            StdOut.println(next[first] + " " + inputSortedChar[lastId]);
+            lastId = next[lastId];
+        }
+
+
+        BinaryStdOut.close();
     }
 
     // if args[0] is "-", apply Burrows-Wheeler transform
 // if args[0] is "+", apply Burrows-Wheeler inverse transform
     public static void main(String[] args) {
-        transform();
+
+        if (args[0].equals("-")) {
+
+            BurrowsWheeler.transform();
+        }
+        if (args[0].equals("+")) {
+            BurrowsWheeler.inverseTransform();
+        }
     }
 }
